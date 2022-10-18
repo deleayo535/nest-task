f0e49b23788f6f85396e49b41c64a15705deceda
Ayodele Afolabi <105075157+deleayo535@users.noreply.github.com>
2022-10-18 10:32:04 +0100

    first commit

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+// import * as uuid from 'uuid/v1';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto) {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+}
